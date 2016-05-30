375efadc9fdac5235a1fbcbaca792d857e8c9ee1
kmahesh-sailfish <maheshvrsecit@gamil.com>
2016-05-30 19:11:16 +0530

    Initial commitdfd

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+var UploadController = function ($scope, fileReader) {
+    console.log(fileReader)
+    $scope.getFile = function () {
+        $scope.progress = 0;
+        var stringsa =0;
+        fileReader.readAsDataUrl($scope.file, $scope)
+            .then(function(result) {
+               $scope.imageSrc = result;
+                window.alert(result);
+
+            });
+    };
+
+    $scope.$on("fileProgress", function(e, progress) {
+        $scope.progress = progress.loaded / progress.total;
+    });
+
+};
+
+app.directive("ngFileSelect",function(){
+
+    return {
+        link: function($scope,el){
+
+            el.bind("change", function(e){
+
+                $scope.file = (e.srcElement || e.target).files[0];
+                $scope.getFile();
+                console.log( $scope.file)
+            })
+
+        }
+
+    }
+
+
+})
